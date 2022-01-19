792599784be4bfdf097d05c2f3d027a2f7c4c4a0
ik012982i9 <ik012982i9@220-15-HZ307533.H012982.NET>
2022-01-19 11:59:43 +0100

    La aplicacion sin las pruebas

---
+++ src/eje3/multiplos.java
@@ -0,0 +1,46 @@
+package eje3;
+
+public class multiplos {
+	
+	public static void main(String[] args) {
+		
+		int n = leerN();
+		int m = leerM(n);
+		int multiplo = 1;
+		while (multiplo < m) {
+			multiplo=siguienteMultiplo(n, multiplo);
+			System.out.println(multiplo);
+			
+		}
+	}
+
+	private static int siguienteMultiplo(int n, int mayorQue) {
+		// TODO Auto-generated method stub
+		int aux= mayorQue+1;
+		while(aux%n!=0) {
+			aux++;
+		}
+		return aux;
+	}
+
+	private static int leerM(int n) {
+		// TODO Auto-generated method stub
+		int m=0;
+		while (m <= n) {
+			System.out.println("Introduce un numero mayor que: " + n + " para M: ");
+			m = Console.readInt();
+		}
+		return m;
+	}
+
+	private static int leerN() {
+		// TODO Auto-generated method stub
+		int n=0;
+		while (n <= 0) {
+			System.out.println("Introduce un entero positivo para N: ");
+			n = Console.readInt();
+		}
+		return n;
+	}
+	
+}
